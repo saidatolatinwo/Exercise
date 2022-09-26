7cb6f5500ada4d3211e4590303f2385751a5cf87
saidatolatinwo <saidatolatinwo2020@gmail.com>
2022-09-26 13:36:57 +0100

    created the html file

---
+++ Exercise.js
@@ -0,0 +1,18 @@
+const person = {
+    Name: "Saidat",
+    Age: 20,
+    Height: "6inches",
+    Weight: "50kg",
+    Gender: "Female",
+    Complexion: "Black",
+    Hobbies: "Sleeping, Listening to Music",
+    FavoriteFood: "Rice",
+    Qualifications: "Bsc",
+    School: "Kwara State University",
+    StateOfOrigin: "Offa" 
+}
+person.Complexion="Fair";
+person.FavoriteFood="Yam";
+person.Ambition="Accountant";
+person.Occupation="Self Employed";
+console.log(person)
